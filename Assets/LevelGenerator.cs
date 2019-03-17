df4b1807cecb64a1dee904095b85f0d88172b163
ryseymour <ry.seymour@gmail.com>
2019-03-16 21:31:28 -0400

    update

---
+++ Assets/LevelGenerator.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    public Texture2D map;
+
+    public ColortoPrefab[] colorMappings;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateLevel();
+    }
+
+    void GenerateLevel ()
+    {
+        for(int x = 0; x < map.width; x++)
+        {
+            for (int y = 0; y < map.height; y++)
+            {
+                GenerateTile(x, y);
+            }
+        }
+    }
+
+    void GenerateTile (int x, int y)
+    {
+        Color pixelColor = map.GetPixel(x, y);
+
+        if (pixelColor.a == 0)
+        {
+            return;
+        }
+
+        foreach (ColortoPrefab colorMapping in colorMappings)
+        {
+            if (colorMapping.color.Equals(pixelColor))
+            {
+                Vector3 position = new Vector3(x-10, 0, y-10);
+                Instantiate(colorMapping.prefab, position, Quaternion.identity, transform);
+            }
+        }
+    }
+}
